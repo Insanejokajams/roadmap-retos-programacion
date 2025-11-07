4f9a7e9171f07baaf4f66c8c87a406cd74201e78
Guillermo <guiurcr@gmail.com>
2025-11-07 07:32:49 -0600

    Add files via upload

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/reto00_guillermo.py
@@ -0,0 +1,23 @@
+# URL oficial del lenguaje: https://www.python.org/
+
+# Comentario de una línea
+
+"""
+Comentario
+multilínea
+"""
+
+PI = 3.14159  # constante por convención
+
+# Tipos de datos primitivos
+CONSTANTE = "esta es sólo la manera de escribir su nombre por convencion, Python no tiene constantes"
+cadena = "esto es una string"
+entero = 5
+flotante = 4.2
+complejo = 4j
+booleano = False
+lista = [1,dos,3]
+tupla = (uno,2,tres)
+diccionario = {"valor1":1,"valor2":"dos"}
+
+print("¡Hola, Python!")
